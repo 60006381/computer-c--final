0c9caa2bf72f7f9990cd72d70c001c3ffb26bdac
60006381 <31143038+60006381@users.noreply.github.com>
2018-05-03 14:27:15 -0600

    Add files via upload

--- calander.cpp
+++ calander.cpp
@@ -2,23 +2,57 @@
 // 5/1/18
 #include <iostream>
 #include <string>
+#include <fstream>
+
 
 using namespace std;
 int main(int argc, char *argv[]) {
 	
+	//declare
 	string date = "";
 	string event = "";
 	string adEvent = "";
+	string moEvent = "";
 	bool isFound = false;
 	ifstream inFile;
 	
 	// prompt
 	
-	cout << "Enter the number of the day(enter - 1 to quite: ";
+	cout << "Enter the number of the day(enter - 1 to quite): ";
 	cin >> date;
 	
-	while(date != "-1")
-	inFile.open("");
+	while(date != "-1"){
+		inFile.open("DATES.txt");
+		while(!inFile.eof()){
+			getline(inFile, date, '#');
+			getline(inFile, event, '#');
+			getline(inFile, adEvent);
 	
+		if(inFile.is_open()){
+			cout << "Date: " << event << endl << endl;
+			
+			
+			cout << adEvent << endl << endl;
+				cout << "additional events - ";
+				cin >> moEvent;
+
+	
+			inFile.close();
+			
+			if(!isFound){
+				cout << "This date does not exist, stop pissing around" << endl;
+			}
+			
+		}
+		else{
+			cout << "file could not be found:" << endl;
+		}
+		}
+		
+		cout << "Enter the number of the day(enter - 1 to quite): ";
+			cin >> date;
+			cout << endl;
+			isFound = false;
+	}
 
 }
