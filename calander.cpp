696ac94118863073e54186411923c12890f38b78
60006381 <31143038+60006381@users.noreply.github.com>
2018-05-03 13:38:07 -0600

    Add files via upload

---
+++ calander.cpp
@@ -0,0 +1,24 @@
+// Johanthan and David
+// 5/1/18
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+	string date = "";
+	string event = "";
+	string adEvent = "";
+	bool isFound = false;
+	ifstream inFile;
+	
+	// prompt
+	
+	cout << "Enter the number of the day(enter - 1 to quite: ";
+	cin >> date;
+	
+	while(date != "-1")
+	inFile.open("");
+	
+
+}
